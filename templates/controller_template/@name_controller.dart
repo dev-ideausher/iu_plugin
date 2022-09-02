859ae2a363e4cef383f90a2c375ca87c7984bd1e
Aman Ali <45947120+amigma00@users.noreply.github.com>
2022-09-02 14:22:31 +0530

    Update @name_controller.dart

--- templates/controller_template/@name_controller.dart
+++ templates/controller_template/@name_controller.dart
@@ -1,11 +1,29 @@
+import 'package:sizer/sizer.dart';
 
-class @NameController  {
-  // Controller code
-  //...
-  //...
-  //...
-  //...
-  //...
-  //...
-  //i am loving it
+extension ResponsiveSize on num {
+  static const double scaleFactorH = 0.123; //100/devide screen height
+  static const double scalefactorW = 0.266; //100/device screen width
+
+  /// Calculates the height depending on the device's screen size
+  ///
+  /// Eg: 20.h -> will take 20% of the screen's height
+  double get h => this * SizerUtil.height / 100;
+
+  /// Calculates the width depending on the device's screen size
+  ///
+  /// Eg: 20.w -> will take 20% of the screen's width
+  double get w => this * SizerUtil.width / 100;
+
+  /// Calculates the sp (Scalable Pixel) depending on the device's screen size
+  double get ksp => this * (SizerUtil.width / 3) / 100;
+
+  /// Calculates the number of pixel depending on the device's screen size
+  ///
+  /// Eg: 20.kh -> will take 20 logical pixel of the screen's height
+  double get kh => (this * SizerUtil.height * scaleFactorH) / 100;
+
+  /// Calculates the number of pixel depending on the device's screen size
+  ///
+  /// Eg: 20.kw -> will take 20 logical pixel of the screen's width
+  double get kw => (this * SizerUtil.width * scalefactorW) / 100;
 }
