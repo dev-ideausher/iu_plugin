7ca80271fd3854164d8812b5e015fd33688395e3
amigmaIU <112845211+amigmaIU@users.noreply.github.com>
2022-09-05 19:46:56 +0530

    Add files via upload

---
+++ templates/controller_template/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'hexColorToFlutterColor.dart';
+
+extension ColorUtil on BuildContext {
+  Color dynamicColor({required int light, required int dark}) {
+    return (Theme.of(this).brightness == Brightness.light)
+        ? Color(light)
+        : Color(dark);
+  }
+
+  Color dynamicColour({required Color light, required Color dark}) {
+    return (Theme.of(this).brightness == Brightness.light) ? light : dark;
+  }
+
+  Color get brandColor1 =>
+      dynamicColour(light: HexColor("#5D48D0"), dark: HexColor("#000000"));
+
+  Color get brandColor2 =>
+      dynamicColour(light: HexColor("#8032A8"), dark: HexColor("#000000"));
+
+}
