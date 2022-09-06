ddcfd66eaaf2131f1a39ca3b3ced490ce6c92b6e
dev-ideausher <100754639+dev-ideausher@users.noreply.github.com>
2022-09-06 12:35:28 +0530

    Update responsiveSize.dart
    add get sizer

--- templates/services_template/responsiveSize.dart
+++ templates/services_template/responsiveSize.dart
@@ -1,4 +1,4 @@
-import 'package:sizer/sizer.dart';
+import 'package:get/get.dart';
 
 extension ResponsiveSize on num {
   static const double scaleFactorH = 0.123; //100/devide screen height
@@ -7,23 +7,23 @@ extension ResponsiveSize on num {
   /// Calculates the height depending on the device's screen size
   ///
   /// Eg: 20.h -> will take 20% of the screen's height
-  double get h => this * SizerUtil.height / 100;
+  double get h => this * Get.height / 100;
 
   /// Calculates the width depending on the device's screen size
   ///
   /// Eg: 20.w -> will take 20% of the screen's width
-  double get w => this * SizerUtil.width / 100;
+  double get w => this * Get.width / 100;
 
   /// Calculates the sp (Scalable Pixel) depending on the device's screen size
-  double get ksp => this * (SizerUtil.width / 3) / 100;
+  double get ksp => this * (Get.width / 3) / 100;
 
   /// Calculates the number of pixel depending on the device's screen size
   ///
   /// Eg: 20.kh -> will take 20 logical pixel of the screen's height
-  double get kh => (this * SizerUtil.height * scaleFactorH) / 100;
+  double get kh => (this * Get.height * scaleFactorH) / 100;
 
   /// Calculates the number of pixel depending on the device's screen size
   ///
   /// Eg: 20.kw -> will take 20 logical pixel of the screen's width
-  double get kw => (this * SizerUtil.width * scalefactorW) / 100;
+  double get kw => (this * Get.width * scalefactorW) / 100;
 }
