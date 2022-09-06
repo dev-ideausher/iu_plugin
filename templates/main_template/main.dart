02b2c4af728d0e5366dcb4a3f55356409eb92e22
dev-ideausher <100754639+dev-ideausher@users.noreply.github.com>
2022-09-06 12:57:53 +0530

    Update main.dart

--- templates/main_template/main.dart
+++ templates/main_template/main.dart
@@ -1,10 +1,7 @@
+import 'package:cli_test/app/modules/home/bindings/home_binding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:get/get.dart';
-import 'package:sizer/sizer.dart';
-
-import 'app/modules/splash/bindings/splash_binding.dart';
 import 'app/routes/app_pages.dart';
 import 'app/services/storage.dart';
 
@@ -18,19 +15,15 @@ Future<void> main() async {
     ],
   );
 
-  return runApp(
-
-        return GetMaterialApp(
-          defaultTransition: Transition.fade,
-          smartManagement: SmartManagement.full,
-          debugShowCheckedModeBanner: false,
-          title: "Ticket Checker",
-          initialRoute: AppPages.INITIAL,
-          initialBinding: SplashBinding(),
-          getPages: AppPages.routes,
-        );
-    
-  );
+  return runApp(GetMaterialApp(
+    defaultTransition: Transition.fade,
+    smartManagement: SmartManagement.full,
+    debugShowCheckedModeBanner: false,
+    title: "Ticket Checker",
+    initialRoute: AppPages.INITIAL,
+    initialBinding: HomeBinding(),
+    getPages: AppPages.routes,
+  ));
 }
 
 Future<void> initGetServices() async {
