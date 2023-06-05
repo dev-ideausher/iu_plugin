ffc488a70d2fca91c4b7c384b268e74cd2f74d90
Aman Ali <112845211+amigmaiu@users.noreply.github.com>
2023-06-05 19:36:41 +0530

    Update dialog_helper.dart

--- templates/services_template/dialog_helper.dart
+++ templates/services_template/dialog_helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'colors.dart';
-import 'responsiveSize.dart';
+import 'responsive_size.dart';
 
 class DialogHelper {
   static void showLoading([String? message]) {
